fd9755b0f8731d57f350e1ccdcfa6392cab98f78
prateek khosla <prateekk96.com@gmail.com>
2020-08-31 01:27:15 +0530

    product middleware and validations

--- controllers/product.js
+++ controllers/product.js
@@ -3,6 +3,7 @@ const {errorHandler}=require('../helpers/dbErrorhandler')
 const formidable=require('formidable');
 const fs=require('fs')
 const lodash =require('lodash');
+
 exports.create=(req,res)=>{
    const form = new formidable.IncomingForm();
    form.keepExtensions=true
@@ -12,8 +13,21 @@ exports.create=(req,res)=>{
                error:"image could not be uploaded"
            })
        }
+       //check for all fields
+       const{name,description,price,category,quantity,shipping}=fields
+       if(!name||!description||!price||!category||!quantity||!shipping){
+           return res.status(400).json({
+               error:"all fields are required"
+           })
+       }
        const product= new Product(fields);
        if(files.photo){
+           console.log(files.photo)
+           if(files.photo.size>2000000){
+               return res.status(400).json({
+                   error:'sile size should be of maximium 2mb'
+               })
+           }
            product.photo.data = fs.readFileSync(files.photo.path)
            product.photo.contentType=files.photo.type
        }
@@ -26,4 +40,20 @@ exports.create=(req,res)=>{
         res.json(data)
     })
    })
+}
+
+exports.productfindById=(req,res,next,id)=>{
+    Product.findById(id).exec((err,product)=>{
+        if(err||!product){
+            return res.status(400).json({
+                error:"product dosent exist"
+                })
+        }
+        req.product=product
+        next();
+    })
+}
+exports.read=(req,res)=>{
+    req.product.photo=undefined
+    return res.json(req.product)
 }
--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,11 @@
 const express=require('express');
 const router=express.Router();
-const{create}=require('../controllers/product')
+const{create,productfindById,read}=require('../controllers/product')
 const{userfindById}=require('../controllers/user')
 const{requireSignin,isAuth,isAdmin}=require('../controllers/auth')
+router.get('/product/:productId',read)
 router.post('/product/create/:userId',requireSignin,isAuth,isAdmin,create);
 
 router.param('userId',userfindById)
+router.param('productId',productfindById)
 module.exports=router;
