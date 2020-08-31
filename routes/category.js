7e093ac7bf2bb826f5f20edb46185863ad887b56
prateek khosla <prateekk96.com@gmail.com>
2020-09-01 01:26:39 +0530

    category update delete extract

--- routes/category.js
+++ routes/category.js
@@ -1,9 +1,12 @@
 const express=require('express');
 const router=express.Router();
-const{create,categorybyId,read}=require('../controllers/category')
+const{create,categorybyId,read,update,del,list}=require('../controllers/category')
 const{userfindById}=require('../controllers/user')
 const{requireSignin,isAuth,isAdmin}=require('../controllers/auth')
 router.post('/category/create/:userId',requireSignin,isAuth,isAdmin,create);
+router.put('/category/:categoryId/:userId',requireSignin,isAuth,isAdmin,update);
+router.delete('/category/:categoryId/:userId',requireSignin,isAuth,isAdmin,del);
+router.get("/categories",list)
 router.get('/category/:categoryId/:userId',read)
 router.param('categoryId',categorybyId)
 router.param('userId',userfindById)
