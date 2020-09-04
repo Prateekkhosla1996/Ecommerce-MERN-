919df7a1d11fd0f80c16fbfa4146c83b4344f452
prateek khosla <prateekk96.com@gmail.com>
2020-09-05 02:37:38 +0530

    user info read update

--- controllers/user.js
+++ controllers/user.js
@@ -11,3 +11,21 @@ exports.userfindById=(req,res,next,id)=>{
         next();
     })
 }
+exports.read=(req,res)=>{
+    req.profile.hashed_password=undefined;
+    req.profile.salt=undefined;
+    return res.json(req.profile);
+
+}
+exports.update=(req,res)=>{
+    User.findByIdAndUpdate({_id:req.profile._id},{$set:req.body},{new:true},(err,user)=>{
+        if(err){
+          return  res.status(400).json({
+              error:'you are not authorize to perform this operation'
+          })
+        }
+        user.hashed_password=undefined
+        user.salt=undefined
+        res.json(user);
+    })
+}
